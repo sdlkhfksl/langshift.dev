b52b8f0a4c599e6758a588110e701dad32b65acb
erweixin <2991552132@qq.com>
2026-04-28 16:25:38 +0800

    feat: add Head component to include Google Ads script for enhanced monetization

---
+++ app/head.tsx
@@ -0,0 +1,12 @@
+export default function Head() {
+  return (
+    <>
+      <script
+        async
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6626163368511917"
+        crossOrigin="anonymous"
+      />
+    </>
+  )
+}
+
